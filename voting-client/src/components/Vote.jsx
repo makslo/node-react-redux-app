410002e4426bc9c5ad5fd8a0f7e17144eeaa45ff
Maxim Lakin <maximlakin@gmail.com>
2016-03-06 21:50:59 -0800

    fixes

--- voting-client/src/components/Vote.jsx
+++ voting-client/src/components/Vote.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
+import classNames from 'classnames'
 
 export default React.createClass({
+  mixins: [PureRenderMixin],
   getPair: function() {
     return this.props.pair || [];
   },
@@ -14,6 +17,7 @@ export default React.createClass({
     return <div className="voting">
       {this.getPair().map(entry =>
         <button key={entry}
+                className={classNames({voted: this.hasVotedFor(entry)})}
                 disabled={this.isDisabled()}
                 onClick={() => this.props.vote(entry)}>
           <h1>{entry}</h1>
